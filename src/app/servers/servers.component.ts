276b914875d504dc48c63a6ba59eb1c99f89726b
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-02-25 15:34:52 -0700

    practicing with property and event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,19 +5,35 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]', // attribute selector: <div app-servers></div>
   // selector: '.app-servers', // class selector: <div class="app-servers"></div>
   // selector: '#app-servers', // id selector is NOT supported
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   `,
   styleUrls: ['./servers.component.css'],
   // styles: ['h3 { color: pink }'] // can also use inline styles
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = "No server was created!";
+  serverName: string = '';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+   }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
+    // console.log("click click!");
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log((<HTMLInputElement>event.target).value);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
