843ea45f22ebdfcea2dec00b9a6c44cef2972d41
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-02-24 17:05:44 -0700

    praciticing styling and selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // typically use this selector for components
+  // selector: '[app-servers]', // attribute selector: <div app-servers></div>
+  // selector: '.app-servers', // class selector: <div class="app-servers"></div>
+  // selector: '#app-servers', // id selector is NOT supported
   // templateUrl: './servers.component.html',
   template: `
     <app-server></app-server>
     <app-server></app-server>
     `,
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  // styles: ['h3 { color: pink }'] // can also use inline styles
 })
 export class ServersComponent implements OnInit {
 
